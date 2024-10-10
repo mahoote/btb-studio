abc5674f6a0a980fb9672a2669c5098526a61f5e
mahoote <martin@teigen.biz>
2024-10-10 02:02:21 +0200

    Persists the state in local storage

--- src/hooks/useNewGameStore.ts
+++ src/hooks/useNewGameStore.ts
@@ -7,32 +7,44 @@ import {
 } from '../constants/NEW_GAME_FORM_DATA'
 import { initialWritingSettingsData } from '../constants/WRITING_SETTINGS_DATA'
 import { initialAdvancedSettingsData } from '../constants/ADVANCED_SETTINGS_DATA'
+import { persist } from 'zustand/middleware'
 
-export const useNewGameStore = create<NewGameState>(set => ({
-    createdGame: null,
-    setCreatedGame: game => set({ createdGame: game }),
+export const useNewGameStore = create<NewGameState>()(
+    persist(
+        set => ({
+            createdGame: null,
+            setCreatedGame: game => set({ createdGame: game }),
 
-    descriptions: initialNewGameData.descriptions,
-    setDescriptions: descriptions => set({ descriptions }),
+            descriptions: initialNewGameData.descriptions,
+            setDescriptions: descriptions => set({ descriptions }),
 
-    selectedAccessories: initialAccessoriesData,
-    setSelectedAccessories: accessories => set({ selectedAccessories: accessories }),
+            selectedAccessories: initialAccessoriesData,
+            setSelectedAccessories: accessories => set({ selectedAccessories: accessories }),
 
-    selectedGameTypes: initialGameTypesData,
-    setSelectedGameTypes: gameTypes => set({ selectedGameTypes: gameTypes }),
+            selectedGameTypes: initialGameTypesData,
+            setSelectedGameTypes: gameTypes => set({ selectedGameTypes: gameTypes }),
 
-    actionCardSettingsData: undefined,
-    setActionCardSettingsData: settings => set({ actionCardSettingsData: settings }),
+            actionCardSettingsData: undefined,
+            setActionCardSettingsData: settings => set({ actionCardSettingsData: settings }),
 
-    actionCardInputs: undefined,
-    setActionCardInputs: inputs => set({ actionCardInputs: inputs }),
+            actionCardInputs: undefined,
+            setActionCardInputs: inputs => set({ actionCardInputs: inputs }),
 
-    activeFormRef: null,
-    setActiveFormRef: ref => set({ activeFormRef: ref }),
+            activeFormRef: null,
+            setActiveFormRef: ref => set({ activeFormRef: ref }),
 
-    writingSettingsData: initialWritingSettingsData,
-    setWritingSettingsData: settings => set({ writingSettingsData: settings }),
+            writingSettingsData: initialWritingSettingsData,
+            setWritingSettingsData: settings => set({ writingSettingsData: settings }),
 
-    advancedSettingsData: initialAdvancedSettingsData,
-    setAdvancedSettingsData: settings => set({ advancedSettingsData: settings }),
-}))
+            advancedSettingsData: initialAdvancedSettingsData,
+            setAdvancedSettingsData: settings => set({ advancedSettingsData: settings }),
+        }),
+        {
+            name: 'new-game-storage',
+            partialize: state => ({
+                ...state,
+                activeFormRef: undefined, // Exclude non-serializable values like refs
+            }),
+        }
+    )
+)
