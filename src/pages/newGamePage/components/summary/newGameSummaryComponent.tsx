677fa164871ed10d9b6b41e8b97343b390785e88
mahoote <martin@teigen.biz>
2024-11-27 22:27:38 +0100

    Add new game table summary

---
+++ src/pages/newGamePage/components/summary/newGameSummaryComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import NewGameSummaryTableComponent from './newGameSummaryTableComponent'
+
+const NewGameSummaryComponent = () => {
+    return (
+        <Box>
+            <NewGameSummaryTableComponent />
+        </Box>
+    )
+}
+
+export default NewGameSummaryComponent
