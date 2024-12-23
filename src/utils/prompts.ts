5012e18c266a1251d67515e71f6c5b47aac315cf
Martin Hoff Teigen <martin@teigen.dev>
2024-12-23 01:45:55 +0100

    Update the generateTranslationPrompt (#66)
    Co-authored-by: mahoote <martin@teigen.biz>

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -19,10 +19,14 @@ import { NewGameTranslations } from '../types/newGame'
 export const generateTranslationPrompt = (languages: string[], data: NewGameTranslations) => {
     const jsonString = JSON.stringify(data, null, 2)
 
-    return `Translate to ${languages.join(', ')} in one object in the format:
-            "{
-                ${languages.map(language => `${language}:{...}`).join(',\n')}
-            }"
-            Here is the data:
-            ${jsonString}`
+    return `Can you translate this English text into ${languages.join(', ')} in a casual and natural way? 
+The translation should sound like something a person from that/those countries would say in a 
+relaxed conversation, without being too stiff or formal. Keep the essence and humor, 
+but adjust the language to make it feel typically for that/those languages.
+Translate to one object in the format:
+"{
+    ${languages.map(language => `${language}:{...}`).join(',\n')}
+}"
+Here is the data:
+${jsonString}`
 }
